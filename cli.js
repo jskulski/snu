fa885282109de14b6317ab5bb445c598bf86a816
Jon Skulski <jskulski@gmail.com>
2016-06-11 17:42:34 -0700

    - Abstraction for parsing any service (with JSON)
    - Adding ability to parse Github

--- cli.js
+++ cli.js
@@ -7,31 +7,63 @@ const chalk = require('chalk');
 
 const snu = require('./snu');
 
-// TODO: better name for indicator on our side?
-function mapColor(indicator) {
-  const map = {
-    'none': snu.Color('green'),
-    'minor': snu.Color('yellow'),
-    'major': snu.Color('red'),
-  }
-  return map[indicator] ? map[indicator] : Color('black');
-}
-
-// parseJSON :: StatusPageIOJSON -> Indicator
-function parseJSON(status) {
-  const color = mapColor(status.status.indicator);
-  return snu.Indicator(status.page.name, color);
-}
-
-// data Parser = Promise -> Indicator
-// data URL String
-// data Service = String | URL | Parser
+// data Key = String
+// data URL = String
+// data Parser = JSON -> Indicator
+// data Service = Key | URL | Parser
 function Service(key, url, parser) {
+  invariant(key, 'Service needs valid key');
+  invariant(url, 'Service needs valid url');
+  invariant(parser, 'Service needs valid parser');
+
   return {
     'key': key,
     'url': url,
-    'parseJSON': parseJSON
+    'parser': parser
   }
+}
+
+// data GithubService = Key | Url
+function GithubService(key, url) {
+  // TODO: better name for indicator on our side?
+  function _mapColor(indicator) {
+    const map = {
+      'good': snu.Color('green'),
+      'minor': snu.Color('yellow'),
+      'major': snu.Color('red'),
+    }
+    return map[indicator] ? map[indicator] : Color('black');
+  }
+
+  // parseJSON :: GithubJSON -> Indicator
+  function _parseJSON(status) {
+    const parseColor = R.compose(_mapColor, R.prop('status'), R.head)
+    return snu.Indicator('Github', parseColor(status))
+  }
+
+  return Service(key, url, _parseJSON);
+}
+
+// data StatuspageIOService = Key | Url
+function StatuspageIOService(key, url) {
+  // TODO: better name for indicator on our side?
+  function _mapColor(indicator) {
+    const map = {
+      'none': snu.Color('green'),
+      'minor': snu.Color('yellow'),
+      'major': snu.Color('red'),
+    }
+    return map[indicator] ? map[indicator] : Color('black');
+  }
+
+  // parseJSON :: StatusPageIOJSON -> Indicator
+  function _parseJSON(status) {
+    const color = _mapColor(status.status.indicator);
+    return snu.Indicator(status.page.name, color);
+  }
+
+  return Service(key, url, _parseJSON);
+
 }
 
 // renderToConsole :: Indicator -> ConsoleIO (Side effect)
@@ -43,19 +75,15 @@ function renderToConsole(indicator) {
   else {
     console.log(chalk.bold.red('vvvvvvv'));
     console.log(chalk.red(indicator.key, ': NOT OK'));
-    console.log('indicator: ', indicator)
     console.log(chalk.bold.red('^^^^^^'));
   }
 }
 
 // gatherReport :: Renderer -> Service -> Indicator (Side effect)
 function gatherReport(renderer, service) {
-  // console.log('renderer', renderer);
-  // console.log('service', service)
   fetch(service.url)
     .then((resp) => resp.json()) // TODO: pass promise around to allow other methods than JSON?
-    .then(service.parseJSON)
-    // .then(t => console.log('test', t))
+    .then(service.parser)
     .then(renderer)
     .catch((error) => console.log('error fetching '+ service.key +': ' + error));
 }
@@ -64,21 +92,22 @@ function gatherReport(renderer, service) {
 function go(config) {
 
   const allServices = [
-    Service('aptible', 'http://status.aptible.com/index.json'),
-    Service('quay', 'http://status.quay.io/index.json'),
-    Service('circleci', 'https://circleci.statuspage.io/index.json'),
-    Service('vimeo', 'http://www.vimeostatus.com/index.json'),
-    Service('travisci', 'https://www.traviscistatus.com/index.json'),
-    Service('uservoice', 'https://status.uservoice.com/index.json'),
-    Service('hipchat', 'https://status.hipchat.com/index.json'),
-    Service('newrelic', 'https://status.newrelic.com/index.json'),
-    Service('bitbucket', 'http://status.bitbucket.org/index.json'),
-    Service('disqus', 'https://status.disqus.com/index.json'),
-    Service('kickstarter', 'http://status.kickstarter.com/index.json'),
-    Service('kmstatus', 'https://kmstatus.com/index.json'),
-    Service('gotomeeting', 'http://status.gotomeeting.com/index.json'),
-    Service('parse', 'https://status.parse.com/index.json'),
-    Service('twilio', 'https://status.twilio.com/index.json')
+    GithubService('github', 'https://status.github.com/api/messages.json'),
+    StatuspageIOService('aptible', 'http://status.aptible.com/index.json'),
+    StatuspageIOService('quay', 'http://status.quay.io/index.json'),
+    StatuspageIOService('circleci', 'https://circleci.statuspage.io/index.json'),
+    StatuspageIOService('vimeo', 'http://www.vimeostatus.com/index.json'),
+    StatuspageIOService('travisci', 'https://www.traviscistatus.com/index.json'),
+    StatuspageIOService('uservoice', 'https://status.uservoice.com/index.json'),
+    StatuspageIOService('hipchat', 'https://status.hipchat.com/index.json'),
+    StatuspageIOService('newrelic', 'https://status.newrelic.com/index.json'),
+    StatuspageIOService('bitbucket', 'http://status.bitbucket.org/index.json'),
+    StatuspageIOService('disqus', 'https://status.disqus.com/index.json'),
+    StatuspageIOService('kickstarter', 'http://status.kickstarter.com/index.json'),
+    StatuspageIOService('kmstatus', 'https://kmstatus.com/index.json'),
+    StatuspageIOService('gotomeeting', 'http://status.gotomeeting.com/index.json'),
+    StatuspageIOService('parse', 'https://status.parse.com/index.json'),
+    StatuspageIOService('twilio', 'https://status.twilio.com/index.json')
   ];
 
   var requestedServices;
