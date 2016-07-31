34268f98df55ade267c51a90019be6ff18aea24e
Jon Skulski <jskulski@gmail.com>
2016-07-31 14:21:39 -0700

    Removing unused spec file

--- spec/services_spec.js
+++
@@ -1,8 +0,0 @@
-const nock = require('nock');
-const assert = require('chai').assert;
-const sinon = require('sinon');
-
-const gatherReport = require('../src/snu').gatherReport;
-const Indicator = require('../src/data').Indicator;
-const Color = require('../src/data').Color;
-const shouldBe = require('./services/common').shouldBe;
